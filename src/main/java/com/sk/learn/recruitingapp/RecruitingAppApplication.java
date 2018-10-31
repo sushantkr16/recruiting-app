4a2f17560ed3031be381194d8ad708587ffea15c
Sushant <sushantkr16@gmail.com>
2018-10-31 22:48:34 +0530

    initial check in

---
+++ src/main/java/com/sk/learn/recruitingapp/RecruitingAppApplication.java
@@ -0,0 +1,12 @@
+package com.sk.learn.recruitingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitingAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sk/learn/recruitingapp/resource/InterviewResource.java
@@ -0,0 +1,10 @@
+package com.sk.learn.recruitingapp.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sk on 27/08/17.
+ */
+@RestController
+public class InterviewResource {
+}
